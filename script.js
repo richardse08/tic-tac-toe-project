c2495c3142e1cdc436b3803f96254077cc924872
richardse08 <eric.richards.finance@gmail.com>
2017-05-09 19:46:42 -0400

    updated html and added some click functionality

--- script.js
+++ script.js
@@ -2,6 +2,43 @@
 // See .ttt in CSS - is there easier way to make the page centered left/right
 
 $(document).ready(function(){
+	
+
+	
+	var playerTurn = true;
+	var machineTurn = "";
+	var algorithm = "";
+	var whosTurn = ""; 
+	var buttonStatus = "allowclick"; // Change to inactive once game starts
+	
+
+	$(".button-left").click(function() {
+		if(buttonStatus === "allowclick") {	
+			buttonStatus = "inactive";
+			player = "X";
+			$(".display").html("Click to Restart");
+		};	
+	});
+
+	$(".button-right").click(function() {
+		if(buttonStatus === "allowclick") {
+			buttonStatus = "inactive";
+			player = "O";
+			$(".display").html("Click to Restart");
+		};
+	}); 
+
+    $(".left, .middle, .right").click(function() {
+		if(buttonStatus === "inactive" && playerTurn === true) {
+			$(this).html(player);
+			playerTurn = false; 
+		};
+	}); // End left/middle/right click listener
+	
+	$(".display").click(function() {
+		playerTurn = true;
+		buttonStatus = "allowclick";
+	});
     
     
     
@@ -19,6 +56,46 @@ $(document).ready(function(){
     
     
     
-    
-    
-});
+}); // End of doc ready
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 	if(buttonStatus === "active" && $(".button-left").click()) {
+// 		buttonStatus = "inactive";
+// 		player = "X";
+// 		$(".display").html("Click to Restart");
+// 	};
+	
+// 	if(buttonStatus === "active" && $(".button-right").click()) {
+// 		buttonStatus = "inactive";
+// 		player = "O";
+// 		$(".display").html("Click to Restart");
+// 	};
